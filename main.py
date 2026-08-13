1cff50f375bd2b87178dceb69d5fb5df99107356
sakinaanaa <sakinajariwala4510@gmail.com>
2026-08-13 03:08:22 +0000

    Add student search feature

--- main.py
+++ main.py
@@ -16,6 +16,20 @@ def calculate_average():
     return total / len(students)
 
 
+def search_student(name):
+    for student in students:
+        if student["name"].lower() == name.lower():
+            print("\nStudent Found:")
+            print("Name:", student["name"])
+            print("Enrollment Number:", student["enrollment"])
+            print("Marks:", student["marks"])
+            return
+
+    print("\nStudent not found")
+
+
 display_students()
 
 print("Average Marks:", calculate_average())
+
+search_student("Aisha")
